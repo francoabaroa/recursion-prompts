c7b9a4618ad54bb0e4444c2c906b2dd7208315aa
Franco Abaroa <franco9395@gmail.com>
2016-09-18 22:50:44 -0700

    should return a number test passing

--- src/recursion.js
+++ src/recursion.js
@@ -23,6 +23,7 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // Example: arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  return array[0][0];
 };
 
 // 4. Check if a number is even.
