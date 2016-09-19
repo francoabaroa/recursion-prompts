c2687f0b603e40d5c9d8143c63cc11734043dbb4
Franco Abaroa <franco9395@gmail.com>
2016-09-18 19:02:28 -0700

    should return factorial for non-negative integers -- test passing

--- src/recursion.js
+++ src/recursion.js
@@ -7,7 +7,11 @@
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
 var factorial = function(n) {
-  return n;
+  if (n === 1 || n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n-1);
+  }
 };
 
 // 2. Compute the sum of an array of integers.
