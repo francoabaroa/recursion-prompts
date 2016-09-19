6e4e828450f7ad92f405e2daee6971e41b41dcf0
Franco Abaroa <franco9395@gmail.com>
2016-09-18 19:04:03 -0700

    should return null for negative numbers and should use recursion by calling itself test passing

--- src/recursion.js
+++ src/recursion.js
@@ -7,7 +7,10 @@
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
 var factorial = function(n) {
-  if (n === 1 || n === 0) {
+  if (n === 1 || n === 0 || n < 0) {
+    if (n < 0) {
+      return null;
+    }
     return 1;
   } else {
     return n * factorial(n-1);
